bad33533dace353c386f3044b07c61547b485771
Raghav Sai Cheedalla <raghavsai.cheedalla@dominionenterprises.com>
2018-06-21 18:59:01 -0400

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Raghav Sai Cheedalla on 6/21/18.
+//  Copyright © 2018 Swift Enthusiast. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
